d438461569206dbbbe933575f9e185e01172a117
Christopher Hartmann <dev@chriha.de>
2018-03-31 03:54:56 +0200

    show only share plugin help if in project

--- help.sh
+++ help.sh
@@ -2,5 +2,7 @@
 
 # # # # # # # # # # # # # # # # # # # #
 # show share commands
-printf "SHARE COMMANDS:\n"
-printf "    ${BLUE}share${NORMAL}${HELP_SPACE:5}Share application in your local network.\n"
+if [ -f "$PROJECT_CONFIG" ]; then
+    printf "SHARE COMMANDS:\n"
+    printf "    ${BLUE}share${NORMAL}${HELP_SPACE:5}Share application in your local network.\n"
+fi
